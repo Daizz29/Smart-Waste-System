6ef5d772eee3afa76f04d310b0d3ea3a56473021
Dinh Quang Lam <dinhquanglam@MacBook-Pro-cua-DQLam.local>
2023-01-15 15:12:27 +0700

    :lamdq: 15/01/2023: analysis into specialzed function and tramit state variable

--- wasteBin/wastebin.ino
+++ wasteBin/wastebin.ino
@@ -4,11 +4,11 @@
 #include <Servo.h>     
 
 //Wifi and mqtt_broker
-const char* ssid = "QL iphone";
-const char* password = "Lam123123";
+const char* ssid = "IoT-LAB";
+const char* password = "Vtnet@1812";
 const char* mqtt_server = "171.244.173.204";
 
-WiFiClient espClient;
+WiFiClient espClient; 
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
 #define MSG_BUFFER_SIZE (100)
@@ -16,14 +16,20 @@ unsigned long lastMsg = 0;
 // servo
 Servo myservo; 
 int pos = 0;
+const int servoPin = D3;
 
 
 // waste1
 char waste1[MSG_BUFFER_SIZE];
-String state1;
+char waste1Ctrl[MSG_BUFFER_SIZE];
 int cap1 = 0;
 float long1 = 105.8492;
 float lat1 = 21.02664;
+char state1[6];
+char stateChange[6] = "open";
+char open[5]= "open";
+char close[6]= "close";
+
 
 // waste2
 char waste2[MSG_BUFFER_SIZE];
@@ -35,7 +41,7 @@ float lat2= 21.02926;
 // waste3
 char waste3[MSG_BUFFER_SIZE];
 int cap3 = 25;
-int state3;
+bool state3;
 float long3 = 105.84998;
 float lat3 = 21.028;
 
@@ -66,7 +72,7 @@ const int echoPin = D2;
 long duration;
 float distance;
 int capacity;
-float deepOfWaste = 18 ;
+float deepOfWaste = 18;
 //////////
 
 void setup_wifi() {
@@ -96,7 +102,6 @@ void setup_wifi() {
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
   Serial.print("Message:");
 
   for (int i = 0; i < length; i++) {
@@ -107,23 +112,17 @@ void callback(char* topic, byte* payload, unsigned int length) {
     const bool state = root["state"];
     Serial.println();
     Serial.println(state);
-    
-    if (state == true){
-        for(pos = 0; pos < 180; pos += 1){ 
-        myservo.write(pos);
+    if(state == false){
+      for(int i=0; i <6; i++){
+          stateChange[i] = close[i]; 
+      }      
+      Serial.println("close");
+    }else{
+        for(int i=0; i <6; i++){
+          stateChange[i] = open[i];
         }
-        Serial.println("open"); 
-        delay(150);
-
-    }
-    else{
-        for(pos = 180; pos>=1; pos-=1) {                           
-        myservo.write(pos);
-        }
-        Serial.println("close");
-        delay(150);
-    }
-
+        Serial.println("open");
+      }  
   Serial.println();
   Serial.println("-----------------------");
 }
@@ -140,9 +139,9 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
+      // client.publish("wasteManagement", "hello world");
       // ... and resubscribe
-      client.subscribe("inTopic");
+      client.subscribe("wasteManagement/waste1/control");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -153,55 +152,51 @@ void reconnect() {
   }
 }
 
+void publish() {
+    stateAssign(state1);
+    snprintf (waste1, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\" } " ,lat1,long1,capacity, state1);
+    client.publish("wasteManagement/waste1/status", waste1, true); 
 
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1884);
+    snprintf (waste2, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\"} " ,lat2,long2,cap2, "open");
+    client.publish("wasteManagement/waste2", waste2, true);
 
-  client.setCallback(callback);
+    snprintf (waste3, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\"} " ,lat3,long3,cap3,"open");
+    client.publish("wasteManagement/waste3", waste3, true);
 
-    // wasteBin
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  //////////////
-  // servo
-  myservo.attach(D5);
+    snprintf (waste4, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\"} " ,lat4,long4,cap4, "open");
+    client.publish("wasteManagement/waste4", waste4, true);
 
+    snprintf (waste5, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\"} " ,lat5,long5,cap5, "open");
+    client.publish("wasteManagement/waste5", waste5, true);
+
+    snprintf (waste6, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\": \"%s\"} " ,lat6,long6,cap6, "open");
+    client.publish("wasteManagement/waste6", waste6, true); 
 }
 
-void loop() {
+void wasteBinData(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
 
-  if (!client.connected()) {
-    reconnect();
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Serial.println(distance);
+  if (distance >= 560){
+    distance = 0;
   }
-  client.loop();
+  else if (distance > deepOfWaste && distance < 560){
+    distance = deepOfWaste;
+  }
+  capacity = 100 - (distance/deepOfWaste)*100;
+  delay(500);
 
-  // wasteBin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-
-// Calculating the distance
-distance= duration*0.034/2;
-
-if (distance >= 560){
-  distance = 0;
-}
-else if (distance > deepOfWaste && distance < 560){
-  distance = deepOfWaste;
-}
-capacity = 100 - (distance/deepOfWaste)*100;
-
+  // fake cap of waste2,3,4,5,6
   unsigned long now = millis();
-
   if (now - lastMsg > 3000) {
     lastMsg = now;
     cap1++;
@@ -210,12 +205,6 @@ capacity = 100 - (distance/deepOfWaste)*100;
     cap4+=3;
     cap5+=5;
     cap6+=10;
-
-    state1 = "open"; // stirng
-    state2 = true; // bool
-    state3 = 0;
-    
-
     if(cap1 > 100){
       cap1 = 0;
     }
@@ -234,28 +223,37 @@ capacity = 100 - (distance/deepOfWaste)*100;
     if(cap6 > 100){
       cap6 = 0;
     }
-    
-  
-    snprintf (waste1, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat1,long1,capacity,"open");
-    client.publish("wasteManagement/waste1", waste1, true);  
-    
-    snprintf (waste2, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat2,long2,cap2,"open");
-    client.publish("wasteManagement/waste2", waste2, true);
-
-    snprintf (waste3, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat3,long3,cap3,"open");
-    client.publish("wasteManagement/waste3", waste3, true);
-
-    snprintf (waste4, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat4,long4,cap4,"open");
-    client.publish("wasteManagement/waste4", waste4, true);
-
-    snprintf (waste5, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat5,long5,cap5,"open");
-    client.publish("wasteManagement/waste5", waste5, true);
-
-    snprintf (waste6, MSG_BUFFER_SIZE," { \"latitude\": \"%f\",\"longtitude\": \"%f\",  \"capacity\": \"%d\", \"state\" :\"%s\"} " ,lat6,long6,cap6,"open");
-    client.publish("wasteManagement/waste6", waste6, true);    
-
-    client.subscribe("wasteManagement/waste1/control");
-
-    
   }
+}
+
+void stateAssign(char *state){
+  for(int i=0; i <7; i++){
+    state[i] = stateChange[i];
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  //servo
+  myservo.attach(servoPin);
+  client.setServer(mqtt_server, 1884);
+
+  client.setCallback(callback);
+  
+    // wasteBin
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  //////////////
+
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  wasteBinData();
+  publish();
+  delay (5000);
 }
